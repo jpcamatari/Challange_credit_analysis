9965b40840faa0840c77641a74bb13037822e398
João Paulo Antonio Camatari <jpcamatari@hotmail.com>
2023-08-31 23:50:35 -0300

    CREATE: Routes and base html

--- analyzer_system/urls.py
+++ analyzer_system/urls.py
@@ -5,3 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('form_admin.urls'))
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- form_admin/urls.py
+++ form_admin/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('form', views.dynamic_form_view)
+    path('', views.dynamic_form_view, name='form')
 ]
