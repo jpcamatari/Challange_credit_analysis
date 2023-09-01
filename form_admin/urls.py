a635fe0c8fb299361dc16b6daf1960f38903c3c5
João Paulo Antonio Camatari <jpcamatari@hotmail.com>
2023-09-01 00:46:03 -0300

    FIX: Route templates, and bugs on view

--- form_admin/urls.py
+++ form_admin/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.dynamic_form_view, name='form')
+    path('form/<int:form_id>/', views.dynamic_form_view, name='form')
 ]
--- form_admin/views.py
+++ form_admin/views.py
@@ -5,11 +5,15 @@ def dynamic_form_view(request, form_id):
     dynamic_form = DynamicForm.objects.get(pk=form_id)
 
     if request.method == 'POST':
-        # Processar o formulário enviado pelo usuário
-        pass
+        form_data = {}
+
+        for field in dynamic_form.form_fields.all():
+            field_name = field.field_name
+            form_data[field_name] = request.POST.get(field_name)
+            
     else:
         # Renderizar o formulário com base nos campos associados
         fields = dynamic_form.form_fields.all()
         context = {'dynamic_form': dynamic_form, 'fields': fields}
-        return render(request, 'template.html', context)
+        return render(request, 'form.html', context)
 
